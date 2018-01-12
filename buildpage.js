819f12d3f06438e873833ab905749f2b3103396d
Andreas Louv <andreas@louv.dk>
2018-01-12 23:48:49 +0100

    Initial

---
+++ buildpage.js
@@ -0,0 +1,188 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * HTML Escape input
+ */
+function X(str, Yify)
+{
+	var map = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#x27;',
+		'`': '&#x60;'
+	};
+	str = String(str).replace(/[&<>"'`]/g, (key) => map[key]);
+
+	return Yify ? Y(str) : str;
+}
+
+/**
+ * Interpret special doc markers
+ */
+function Y(str)
+{
+	/* {@link $REF} */
+	return str.replace(/{@link\s+(\S+)}/g, (_, ref) => {
+		return `<a href="#ref:${X(ref)}">${X(ref)}</a>`;
+	});
+}
+
+function buildpage(outfp, docs)
+{
+	const lines = docs.content.split(/\r?\n/);
+	if (lines[lines.length - 1] == '')
+		lines.pop();	/* Not a line, but a line terminator */
+
+	outfp.write(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://unpkg.com/chota@latest">
+	<style type="text/css">
+		input {
+			margin: 0 0 40px;
+			width: 100%;
+		}
+		section, h1 {
+			padding: 20px 0 0;
+		}
+		.container {
+			max-width: 800px;
+			margin: 0 auto;
+		}
+		.code td, pre {
+			padding-top: 0;
+			padding-bottom: 0;
+			margin-top: 0;
+			margin-bottom: 0;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h1>${X(docs.file.split('/').pop())}</h1>
+		<input type="search" placeholder="Search" onchange="S(this.value)">
+	</div>
+	<hr>
+	`);
+	docs.tags.forEach((doc, index) => {
+		outfp.write(`
+	<section><div class="container">
+		<h2 title="${X(doc.short_desc, false)}">
+			<a name="ref:${X(doc.refname)}" href="#ref:${X(doc.refname)}">${X(doc.refname)}</a>
+			<a href="#line:${X(doc.line)}" title="View Source">[S]</a>
+			-
+			${X(doc.short_desc)}
+		</h2>
+		${Y(doc.long_desc)}
+		`);
+
+		if (doc.tag_params.length || doc.tag_return.desc) {
+			outfp.write(`
+		<table>
+			<thead>
+				<tr>
+					<th></th>
+					<th>Type</th>
+					<th>Description</th>
+				</tr>
+			</thead>
+			<tbody>
+			`);
+			if (doc.tag_return.desc) {
+				outfp.write(`
+				<tr>
+					<td><strong>Return Value<strong></td>
+					<td>${X(doc.tag_return.type)}</td>
+					<td>${X(doc.tag_return.desc)}</td>
+				</tr>
+				`);
+			}
+			doc.tag_params.forEach((param) => {
+				outfp.write(`
+				<tr>
+					<td><a href="#line:${X(param.line)}">${X(param.arg)}</a></td>
+					<td>${X(param.type)}</td>
+					<td>${X(param.desc)}</td>
+				</tr>
+				`);
+			});
+			outfp.write(`
+			</tbody>
+		</table>
+			`);
+		}
+
+		if (doc.tag_see.length) {
+			outfp.write(`
+		<h4>See Also</h4>
+		<ul>
+			`);
+			doc.tag_see.forEach((ref) => {
+				outfp.write(`
+			<li><a href="#ref:${X(ref)}">${X(ref)}</a>
+				`);
+			});
+			outfp.write(`
+		</ul>
+			`);
+		}
+
+		outfp.write(`
+	</div></section>
+		`);
+	});
+	outfp.write(`
+	<section><div class="container">
+		<table class="code">
+			<tbody>
+	`);
+	lines.forEach((line, lineno) => {
+		lineno++;
+		outfp.write(`
+				<tr>
+					<td align="right"><pre><a name="line:${X(lineno)}" href="#line:${X(lineno)}">${X(lineno)}</a></pre></td>
+					<td><pre>${line ? X(line) : '&nbsp;'}</pre></td>
+				</tr>
+		`);
+	});
+	outfp.write(`
+			</tbody>
+		</table>
+	</div></section>
+	<script>
+		let query = location.search.slice(1);
+		if (query) {
+			let I = document.querySelector('input');
+			I.value = query;
+			search(query);
+		}
+		function S(query) {
+			location.href = '?' + encodeURIComponent(query);
+		}
+		function search(query) {
+			query = query.toLowerCase();
+			[].map.call(document.querySelectorAll('h2'), (el) => {
+				while (el && el.nodeName.toLowerCase() != 'section')
+					el = el.parentNode;
+				return el;
+			}).forEach((section) => {
+				if (section.innerHTML.toLowerCase().includes(query))
+					section.style.display = 'block';
+				else
+					section.style.display = 'none';
+			});
+		};
+	</script>
+</body>
+</html>
+	`);
+}
+module.exports = buildpage;
---
+++ indexfile.js
@@ -0,0 +1,222 @@
+"use strict";
+
+const fs = require('fs');
+
+function isspace(c)
+{
+	return (c == '\t' || c == ' ');
+}
+
+function indexfile(infile, callback)
+{
+	let content = fs.readFileSync(infile).toString();
+
+	const docs = {
+		file: infile,
+		fileid: infile.replace(/\//g, '%'),
+		tags: [],
+		content: content
+	};
+	let doc;
+
+	const scope = {
+		proc: '',
+		ds: '',
+		pi: ''
+	};
+
+	const DOC_OUT    = 0;	/* Searching for documentation */
+	const DOC_NAME   = 1;	/* Searching for a reference name */
+	const DOC_SDESC  = 2;	/* Short description (First line of documentation) */
+	const DOC_LDESC  = 3;	/* Long description (Continues lines of documentation) */
+	const DOC_SEE    = 4;	/* @see    $REF */
+	const DOC_SEEC   = 5;	/* @see    $REF */
+	const DOC_PARAM  = 6;	/* @param  $ARG $DESC */
+	const DOC_PARAMC = 7;	/* @param  $ARG $DESC */
+	const DOC_RETURN = 8;	/* @return $DESC */
+
+	let state = DOC_OUT;
+
+	content.split(/\r?\n/).forEach((line, lineno) => {
+		lineno++;
+		if (line.length == 0)
+			return;		/* Skip empty lines */
+
+		if (state == DOC_NAME) {
+			let m;
+			if ((m = line.match(/^\s*dcl-(proc|ds|s|c)\s+(\w+)/))) {
+				doc.type = m[1];
+				doc.refname = m[2];
+			} else {
+				doc.type = '';
+				doc.name = '';
+			}
+
+			doc.exported = /\bexport\b/.test(line);
+
+			state = DOC_OUT;
+			/* Fall through */
+		}
+		if (state == DOC_OUT) {
+			if (!scope.pi) {
+				let m;
+				/* We need to capture something to please String.match, hence the
+				 * wierd capturing group below */
+				if ((m = line.match(/^\s*dcl-pi\s+\*n\s*((?:\s+\S*\s*)?;)/i))
+				    && !/\bend-pi\b/.test(line)) {
+					scope.pi = m[1].trim().slice(0, -1) || '-';
+					if (doc && doc.refname == scope.proc)
+						doc.tag_return.type = scope.pi;
+				}
+			} else {
+				if (/^\s*end-pi\b/i.test(line)) {
+					scope.pi = '';
+				} else if (doc && doc.refname == scope.proc) {
+					/* Update current doc with proper types for the params */
+					let m;
+					if ((m = line.match(/^\s*(\w+)\s+(\S+(?=;|\s))/))) {
+						doc.tag_params.some((param) => {
+							if (param.arg == m[1]) {
+								param.type = m[2];
+								param.line = lineno;
+								return true;
+							}
+						});
+					}
+				}
+			}
+
+			if (!scope.ds) {
+				let m;
+				if ((m = line.match(/^\s*dcl-ds\s+(\w+)/i))
+				    && !/\bend-ds\b|\blikeds\b/.test(line))
+					scope.ds = m[1];
+			} else {
+				if (/^\s*end-ds\b/i.test(line)) {
+					scope.ds = '';
+				} else if (doc && doc.refname == scope.ds) {
+					/* Update current doc with proper types for the params */
+					let m;
+					if ((m = line.match(/^\s*(\w+)\s+(\S+(?=;|\s))/))) {
+						doc.tag_params.some((param) => {
+							if (param.arg == m[1]) {
+								param.type = m[2];
+								param.line = lineno;
+								return true;
+							}
+						});
+					}
+				}
+			}
+
+			if (!scope.proc) {
+				let m;
+				if ((m = line.match(/^\s*dcl-proc\s+(\S+)/i)))
+					scope.proc = m[1];
+			} else {
+				if (/^\s*end-proc\b/i.test(line))
+					scope.proc = '';
+			}
+
+			if (/^\s*\/\*\*/.test(line)) {
+				state = DOC_SDESC;
+
+				doc = {
+					line: lineno,
+					scope: Object.assign({}, scope),
+					exported: false,
+					type: '',		/* proc, ds, s, c, or '' */
+					refname: '',		/* procedure, structure, ... name */
+					short_desc: '',		/* one time description */
+					long_desc: '',		/* ...continuesly lines */
+					tag_see: [],		/* @see    $REFNAME */
+					tag_params: [],		/* @param  $ARG $DESC */
+					tag_return: {		/* @return $DESC */
+						type: '-',
+						desc: ''
+					}
+				};
+				docs.tags.push(doc);
+			}
+			return;		/* Next line */
+		}
+
+		if (/\*\/\s*$/.test(line)) {
+			state = DOC_NAME;
+			return;		/* Next line */
+		}
+
+		/* Remove comment leader */
+		line = line.replace(/^\s*\*\s*/gm, '');
+
+		if (line[0] == '@') {
+			if (line.startsWith('@see') && isspace(line[4])) {
+				state = DOC_SEE;
+				line = line.slice(4);
+			} else if (line.startsWith('@param') && isspace(line[6])) {
+				state = DOC_PARAM;
+				line = line.slice(6);
+			} else if (line.startsWith('@return') && isspace(line[7])) {
+				state = DOC_RETURN;
+				line = line.slice(7);
+			} else {
+				throw new Error(line.split(/\s+/).shift() + ': unknown tag');
+			}
+		}
+
+		line = line.trim();
+
+		switch (state) {
+		case DOC_SDESC:
+			doc.short_desc = line;
+			state = DOC_LDESC;
+			break;
+		case DOC_LDESC:
+			if (!doc.long_desc)
+				doc.long_desc = line;
+			else
+				doc.long_desc += ' ' + line;
+			break;
+		case DOC_SEE:
+			doc.tag_see.push(line);
+			state = DOC_SEEC;
+			break;
+		case DOC_SEEC:
+			doc.tag_see[doc.tag_see.length - 1] += ' ' + line;
+			break;
+		case DOC_PARAM:
+			/*
+			 * Read of first word as "arg",
+			 * store the rest in "desc"
+			 */
+			let i;
+			let arg = '';
+			for (i = 0; i < line.length; i++) {
+				if (isspace(line[i]))
+					break;
+				arg += line[i];
+			}
+			doc.tag_params.push({
+				arg: arg,
+				type: '-',
+				line: lineno,
+				desc: line.slice(i).trim()
+			});
+			state = DOC_PARAMC;
+			break;
+		case DOC_PARAMC:
+			doc.tag_params[doc.tag_params.length - 1].desc += ' ' + line;
+			break;
+		case DOC_RETURN:
+			if (!doc.tag_return.desc)
+				doc.tag_return.desc = line;
+			else
+				doc.tag_return.desc += ' ' + line;
+			break;
+		}
+	});
+
+	return docs;
+}
+
+module.exports = indexfile;
