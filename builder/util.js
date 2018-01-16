c0eec50574e79c592a559101e56694cadc089cdf
Andreas Louv <andreas@louv.dk>
2018-01-16 20:12:53 +0100

    Support ``@code'' marker

--- builder/util.js
+++ builder/util.js
@@ -19,10 +19,62 @@ function escapeHtml(str)
  */
 function interpretMarkers(str)
 {
-	/* {@link $REF} */
-	return str.replace(/{@link\s+(\S+)}/g, (_, ref) => {
-		return `<a href="#ref:${escapeHtml(ref)}">${escapeHtml(ref)}</a>`;
-	});
+	const DFT   = 0;
+	const TYPE  = 1;
+	const VALUE = 2;
+
+	let out = '';
+	let state = DFT;
+	let marker;
+
+	for (let i = 0; i < str.length; i++) {
+		if (state == DFT) {
+			if (str[i] == '{' && str[i + 1] == '@') {
+				i += 1;
+				state = TYPE;
+				marker = {
+					type: '',
+					brackets: 1,
+					value: ''
+				};
+			} else {
+				out += str[i];
+			}
+			continue;
+		}
+		switch (state) {
+		case TYPE:
+			if (str[i] !== ' ' && str[i] !== '\t') {
+				marker.type += str[i];
+			} else {
+				state = VALUE;
+			}
+			break;
+		case VALUE:
+			if (str[i] == '}')
+				marker.brackets--;
+			if (str[i] == '{')
+				marker.brackets++;
+
+			if (marker.brackets === 0) {
+				switch (marker.type) {
+				case 'link':
+					out += `<a href="#ref:${marker.value}">${marker.value}</a>`;
+					break;
+				case 'code':
+					out += `<code>${marker.value}</code>`;
+					break;
+				default:
+					throw new Error('@' + marker.type + ': unknown marker');
+				}
+				state = DFT;
+			} else {
+				marker.value += str[i];
+			}
+			break;
+		}
+	}
+	return out;
 }
 
 module.exports = {
